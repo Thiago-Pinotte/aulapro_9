295a39e2a452d4695850878e83852e535dc5bf87
Thiago-Pinotte <107222329+Thiago-Pinotte@users.noreply.github.com>
2023-05-30 18:45:08 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200, 200, 30, 30);
+  
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(RIGHT_ARROW)) {
+    box.position.x +=5;
+    background("red");
+    //box.color("blue")
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    box.position.x -=5;
+    background("blue");
+
+  }
+  if (keyIsDown(UP_ARROW)) {
+    box.position.y -=5;
+    background("yellow");
+
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    box.position.y +=5;
+    background("green");
+
+  }
+
+  drawSprites();
+
+}
+
+
+
+
